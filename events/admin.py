8a66ad929b437cd115c5bb28d1ad7358cfb26176
Dennis433 <dennisbija234@gmail.com>
2025-04-29 22:39:14 +0100

    Registered Category and Event models with Django admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Event
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'location', 'category', 'created_at']
+    list_filter = ['category', 'date']
+    search_fields = ['title', 'description', 'location']
+    date_hierarchy = 'date'
